2adc3f44893a1bd46c7ac48ab8921cd8a15aafc8
Bsheepcoder <18947336207@163.com>
2023-09-21 03:36:38 +0800

    文章页请求bug修复

--- backend/src/main/java/com/backend/entity/Article.java
+++ backend/src/main/java/com/backend/entity/Article.java
@@ -9,12 +9,12 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 public class Article {
-    int aid;
     String title;
     int tag;
     @JsonFormat(pattern = "yyyy-MM-dd")
     Date datetime;
     String overview;
-    int autherid;
     String text;
+    int aid;
+    int autherid;
 }
