b914eaa30fdca8f68bf2c8e3a1e55a45c3efb7ea
Bsheepcoder <18947336207@163.com>
2023-09-20 22:13:02 +0800

    标签接口bug修复

--- backend/src/main/java/com/backend/controller/TagController.java
+++ backend/src/main/java/com/backend/controller/TagController.java
@@ -19,26 +19,36 @@ public class TagController {
     TagService tagService;
 
     @GetMapping("/add")
-    public RestBean<String> addTag(@RequestParam("tag") String tag){
-        if(tagService.addTag(tag)){
+    public RestBean<String> addTag(@RequestParam("tagName") String name){
+        if(tagService.addTag(name)){
             return RestBean.success("添加成功！");
         }
         return RestBean.failure(500,"接口添加失败！");
     }
 
     @GetMapping("/del")
-    public RestBean<String> deleteTag(@RequestParam("tag") String tag){
-
+    public RestBean<String> deleteTag(@RequestParam("tagId") int id){
+        if(tagService.deleteTag(id)){
+            return RestBean.success("删除成功！");
+        }
+        return RestBean.failure(500,"接口删除失败！");
     }
 
     @GetMapping("/up")
-    public RestBean<String> updateTag(@RequestParam("tag") String tag){
-
+    public RestBean<String> updateTag(@RequestParam("tagId") int id,@RequestParam("tagName") String name){
+        if(tagService.updateTag(id,name)){
+            return RestBean.success("更新成功！");
+        }
+        return RestBean.failure(500,"接口更新失败！");
     }
 
     @GetMapping("/list")
-    public RestBean<List<ArticleTag>> getTagList(@RequestParam("tag") String tag){
-
+    public RestBean<List<ArticleTag>> getTagList(){
+        List<ArticleTag>  list  = tagService.getTagList();
+        if(list.size() != 0){
+            return RestBean.success(list);
+        }
+        return RestBean.failure(500,null);
     }
 
 }
