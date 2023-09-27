a7b2d98a1f9de4c7922ee6f6f701faebb63d2d5f
Bsheepcoder <18947336207@163.com>
2023-09-27 23:30:40 +0800

    添加mapper

--- backend/src/main/java/com/backend/mapper/ArticleTagMapper.java
+++ backend/src/main/java/com/backend/mapper/ArticleTagMapper.java
@@ -1,5 +1,6 @@
 package com.backend.mapper;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -12,8 +13,12 @@ public interface ArticleTagMapper {
     @Insert("insert into article_tag(aid,tid) values(#{aid},#{tid})")
     public boolean addTagForArticle(int aid,int tid);
 
+    @Delete("delete from article_tag where aid = #{aid}")
+    public boolean deleteArticleAllTags(int aid);
+
+    @Delete("delete from article_tag where aid = #{aid} and tid = #{tid}")
+    public boolean deleteArticleTag(int aid,int tid);
+
     @Select("select t.tname from tag t inner join article_tag a on t.tid = a.tId where a.aId = #{aid}")
     public List<String> getArticleTagList(int aid);
-
-
 }
